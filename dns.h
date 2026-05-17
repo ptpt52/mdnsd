6e2d36808a73aff48f3ccf90570079d8f20e2308
PTPT52 <ptpt52@users.noreply.github.com>
2026-05-17 21:30:06 +0800

    dns: support additional section and SRV/TXT queries
    When responding to a PTR query, SRV and TXT records should be placed
    in the additional section and not the answer section. When responding
    to an A or AAAA query, addresses of mismatching type should be placed
    in the additional section and not the answer section.
    
    Respond to SRV and TXT queries. Only respond to PTR queries without
    instance specified (instance is not included in name of PTR records).
    PTR/SRV/TXT responses are always forced to yield predictable results.
    Rate limiting is only kept for the legacy ANY query handling.
    
    Fix a bug where the timeout was updated even if the response is forced
    and potential NULL pointer issues.
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>
    Signed-off-by: Erik Karlsson <erik.karlsson@iopsys.eu>

--- dns.h
+++ dns.h
@@ -83,6 +83,7 @@ extern int cfg_no_subnet;
 void dns_packet_init(void);
 bool dns_packet_question(const char *name, int type);
 bool dns_packet_answer(const char *name, int type, const uint8_t *rdata, uint16_t rdlength, int ttl);
+void dns_packet_additional(const char *name, int type, const uint8_t *rdata, uint16_t rdlength, int ttl);
 void dns_packet_send(struct interface *iface, struct sockaddr *to, bool query, int multicast);
 
 void dns_query(const char *name, uint16_t type);
@@ -90,6 +91,7 @@ void dns_query(const char *name, uint16_t type);
 void dns_send_question(struct interface *iface, struct sockaddr *to,
 		       const char *question, int type, int multicast);
 void dns_reply_a(struct interface *iface, struct sockaddr *to, int ttl, const char *hostname, bool append);
+void dns_reply_a_qtype(struct interface *iface, struct sockaddr *to, int ttl, const char *hostname, uint16_t qtype, bool append);
 void dns_reply_a_additional(struct interface *iface, struct sockaddr *to, int ttl, bool append);
 const char* dns_type_string(uint16_t type);
 void dns_handle_packet(struct interface *iface, struct sockaddr *s, uint16_t port, uint8_t *buf, int len);
