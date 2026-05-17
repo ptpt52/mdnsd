da5570e7f60702b7a81036d0107230c1835d2bae
PTPT52 <ptpt52@users.noreply.github.com>
2026-05-17 21:30:06 +0800

    dns: support additional section and SRV/TXT queries
    When responding to a PTR query, SRV and TXT records should be placed
    in the additional section and not the answer section. When responding
    to an A or AAAA query, addresses of mismatching type should be placed
    in the additional section and not the answer section.
    
    Respond to SRV and TXT queries. Only respond to PTR queries without
    instance specified (instance is not included in name of PTR records).
    PTR/SRV/TXT responses are always forced to yield predictable results.
    Rate limiting is only kept for the legacy ANY query handling.
    
    Fix a bug where the timeout was updated even if the response is forced
    and potential NULL pointer issues.
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>
    Signed-off-by: Erik Karlsson <erik.karlsson@iopsys.eu>

--- service.h
+++ service.h
@@ -14,6 +14,7 @@
 #ifndef _SERVICE_H__
 #define _SERVICE_H__
 
+#include <stdint.h>
 #include <libubox/vlist.h>
 #include <libubox/avl-cmp.h>
 
@@ -42,7 +43,7 @@ extern struct vlist_tree announced_services;
 
 extern void service_init(int announce);
 extern void service_cleanup(void);
-extern void service_reply(struct interface *iface, struct sockaddr *to, const char *instance, const char *service_domain, int ttl, int force, bool append);
+extern void service_reply(struct interface *iface, struct sockaddr *to, const char *instance, const char *service_domain, int ttl, int force, uint16_t qtype, bool append);
 extern void service_announce_services(struct interface *iface, struct sockaddr *to, int ttl, bool append);
 extern void service_update(struct vlist_tree *tree, struct vlist_node *node_new, struct vlist_node *node_old);
 
